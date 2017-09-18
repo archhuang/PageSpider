14479cbdd8fcd9b5091a9a0b82eecf82a3501a0a
Arch <arch.huang@gmail.com>
2017-09-18 17:25:15 +0800

    Initial Commit

---
+++ utilities/url_utilities.py
@@ -0,0 +1,13 @@
+def load_urls_from_file(file_path: str):
+    #TODO: Need Implement
+    pass
+
+def load_page(url: str):
+    #TODO: Need Implement
+    pass
+
+def scrape_page(page_contents: str):
+    #TODO: Need Implement
+    pass
+
+
