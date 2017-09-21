34ba36f6dbbe03111677b131ad2784fb264ac373
Arch <arch.huang@gmail.com>
2017-09-21 21:53:43 +0800

    Add Utilities Codes for Database and URL

--- utilities/database_utilities.py
+++ utilities/database_utilities.py
@@ -1,8 +1,31 @@
+import sqlite3 as lite
+
+
 def create_database(database_path: str):
-    # TODO: Need to Implement
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        cur.execute("drop table if EXISTS  words")
+
+        ddl = "create table words( word TEXT not null primary key, usage_count INT default 1 not null);"
+        cur.execute(ddl)
+        ddl = "create unique index words_word_uindex on words (word);"
+        cur.execute(ddl)
+    conn.close()
 
 
 def save_words_to_database(database_path: str, words_list: list):
-    # TODO: Need to Implement
-    pass
+    conn = lite.connect(database_path)
+    with conn:
+        cur = conn.cursor()
+        for word in words_list:
+            sql = "select count(word) from words where word='" + word + "'"
+            cur.execute(sql)
+            count = cur.fetchone()[0]
+            if count > 0:
+                sql = "update words set usage_count = usage_count + 1 where word = '" + word + "'"
+            else:
+                sql = "insert into words(word) values ('" + word + "')"
+            cur.execute(sql)
+        conn.close()
+        print("DB Save Complete.")
